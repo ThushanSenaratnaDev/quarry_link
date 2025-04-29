5ea1107e2ae6a1a5f580b9d37333af1d69354d9f
Thushan Senaratna <thushansenaratne1@gmail.com>
2025-04-29 22:07:30 +0530

    Merge Inventory and Product Management

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,8 @@ const Header = () => {
       <nav className="nav">
         <Link to="/employee-management">Employee Management</Link>
         <Link to="/detonation-planning">Detonation Planning</Link>
-        <Link to="/inventory">Inventory & Product Catalog</Link>
+        <Link to="/product-catalogue">Product Catalog</Link>
+        <Link to="/inventory-control">Inventory Control</Link>
         <Link to="/event-planning">Event Planning</Link>
         <Link to="/order-management">Order Management</Link>
         <Link to="/client-management">Client Management</Link>
